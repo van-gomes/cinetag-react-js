467a2005d4bff226e454da63ebdef50a253879ea
van-gomes <nninha.gomes@gmail.com>
2024-12-08 15:18:23 -0300

    Creating Banner Component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import Footer from "components/Footer/Footer";
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
+import Banner from "components/Banner/Banner";
 
 function Home() {
   return (
     <div>
       <>
+        <Banner imagem="home" />
         <Cabecalho />
         <Footer />
       </>
