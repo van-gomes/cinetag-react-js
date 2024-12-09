c50b0ad5cad14284b032d7cd6582397b559f8d3e
van-gomes <nninha.gomes@gmail.com>
2024-12-09 16:42:18 -0300

    Making Card component dynamic

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,9 @@ import Footer from "components/Footer/Footer";
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import Banner from "components/Banner/Banner";
 import Title from "components/Title/Title";
+import Card from "components/Card/Card";
+import styles from "./Home.module.css";
+import videos from "./db.json";
 
 function Home() {
   return (
@@ -12,6 +15,11 @@ function Home() {
         <Title>
           <h1>Um lugar para guardar seus vídeos e filmes!</h1>
         </Title>
+        <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+        </section>
         <Footer />
       </>
     </div>
