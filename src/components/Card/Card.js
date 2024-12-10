69a6fb2b02f2e86d6a15d20a3ec446aeb63ba6a2
van-gomes <nninha.gomes@gmail.com>
2024-12-10 12:20:35 -0300

    Enabling favorites and unfavorites

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,25 @@
-import styles from './Card.module.css';
-import iconeFavoritar from './favoritar.png';
+import { useFavoritoContext } from "context/FavoritosContext";
+import styles from "./Card.module.css";
+import iconeFavoritar from "./favoritar.png";
+import iconeDesfavoritar from "./desfavoritar.png";
 
 function Card({ id, titulo, capa }) {
+    console.log(id, titulo, capa)
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar}
+            <img src={icone}
                 alt="Favoritar filme"
-                className={styles.favoritar} />
+                className={styles.favoritar}
+                onClick={() => {
+                    adicionarFavorito({ id, titulo, capa })
+                }}
+            />
         </div>
 
     )
