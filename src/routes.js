5f4ee675072c8e860105b3a81c12b6893b1a7975
van-gomes <nninha.gomes@gmail.com>
2024-12-10 09:57:52 -0300

    Creating useFavoritoContext hook

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import Favorite from "./pages/Favorite/Favorite";
 import Footer from "components/Footer/Footer";
 import Cabecalho from "components/Cabecalho/Cabecalho";
 import Container from "components/Container/Container";
+import FavoritosProvider from "context/FavoritosContext";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Cabecalho />
             <Container>
+            <FavoritosProvider>
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/favoritos" element={<Favorite />}></Route>
                 </Routes>
+            </FavoritosProvider>
             </Container>
             <Footer />
         </BrowserRouter>
