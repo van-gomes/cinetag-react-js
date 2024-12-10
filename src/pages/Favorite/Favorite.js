bf49d08968bbe04094b73bacef844150c7f1363c
van-gomes <nninha.gomes@gmail.com>
2024-12-10 12:43:49 -0300

    Favorites list

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,18 +1,23 @@
+import { useFavoritoContext } from "context/FavoritosContext";
 import Banner from "components/Banner/Banner";
 import Title from "components/Title/Title";
 import Card from "components/Card/Card";
 import styles from "./Favorite.module.css";
 
 function Favoritos() {
+    const { favorito } = useFavoritoContext();
+
     <>
-            <Banner imagem='favoritos' />
-            <Title>
-                <h1>Meus Favoritos</h1>
-            </Title>
-            <section className={styles.container}>
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
-            </section>
-        </>
+     <Banner imagem='favoritos' />
+     <Title>
+        <h1>Meus Favoritos</h1>
+    </Title>
+    <section className={styles.container}>
+        {favorito.map((fav) => {
+            return <Card {...fav} key={fav.id} />
+        })}
+    </section> 
+    </>
 }
 
 export default Favoritos;
